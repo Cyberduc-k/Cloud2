3e90288781f361b58adf2b8b920a3cc23cffe133
Cyberduc-k <tttymo@gmail.com>
2023-01-09 15:26:30 +0100

    add environmental variable for rabbitmq connection

--- API/startSudoku/main.go
+++ API/startSudoku/main.go
@@ -179,7 +179,8 @@ func setupMongo(ctx context.Context) (*mongo.Client, error) {
 }
 
 func setupRabbit() (*amqp.Connection, *amqp.Channel, error) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	hostname := os.Getenv("RABBITMQ_CONNECTION")
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://guest:guest@%s:5672", hostname))
 	if err != nil {
 		return nil, nil, err
 	}
