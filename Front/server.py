bc50f43263606f2cce74bf696a8801f66ffd8525
Cyberduc-k <tttymo@gmail.com>
2023-01-13 12:06:00 +0100

    fix kubernetes file

--- Front/server.py
+++ Front/server.py
@@ -4,6 +4,7 @@ import socketserver
 class StaticFileHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.path = f"/static{self.path}"
+        print(self.path)
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 PORT = 8081
