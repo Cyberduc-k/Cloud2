7d757c1419024d6f92f197c39ccb4fc33c2ba7f6
Cyberduc-k <tttymo@gmail.com>
2023-01-13 11:03:11 +0100

    add simple python server

---
+++ Front/server.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+
+class StaticFileHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.path = f"/static{self.path}"
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+PORT = 8081
+handler = StaticFileHttpRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print("Server started at localhost:" + str(PORT))
+    httpd.serve_forever()
